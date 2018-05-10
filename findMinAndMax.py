423083287f13cc17759a76bffb38c33a72d3c765
SSPMII <sspmii@126.com>
2018-05-10 20:51:06 +0800

    findMinAndMax

---
+++ findMinAndMax.py
@@ -0,0 +1,26 @@
+# _*_ coding: utf-8 _*_
+
+def findMinAndMax(L):
+	min = L[0]
+	max = L[0]
+	for x in L:
+		if x > max:
+			max = x
+		else:
+			min = x
+	return (min, max)
+
+
+
+
+# 测试
+if findMinAndMax([]) != (None, None):
+    print('测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败!')
+else:
+    print('测试成功!')

04f747b72d3340d45c261630cbb0b36538e05f55
sspmii <sspmii@126.com>
2018-07-11 00:03:01 +0800

    类和对象

---
+++ TuLingXueYuan/oop/01.py
@@ -0,0 +1,20 @@
+'''
+新建一个学生类
+'''
+class Student():
+	pass
+#定义一个对象
+pengpeng = Student()
+#定义一个Python学生类
+class PythonStudent():
+	name = None
+	age = 18
+	course = 'Python'
+	def doHomework(self):
+		print('我在做作业')
+		#推荐在函数末尾使用return
+		return None
+shenwei = PythonStudent()
+print(shenwei.name)
+print(shenwei.age)
+shenwei.doHomework()

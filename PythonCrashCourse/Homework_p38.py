1d2545af432eb201f00fb4c85af56f3779bbd3e3
sspmii <sspmii@126.com>
2018-07-16 21:36:31 +0800

    新书的学习

---
+++ PythonCrashCourse/Homework_p38.py
@@ -0,0 +1,23 @@
+# p38 列表习题
+guests = ['strike','aegis','Duel','Buster','Freedom','Justice','unicorn']
+for name in guests:
+    print('I will invite {0} to have dinner'.format(name.title()))
+print("\nUnfortunately, {0} won't come".format(guests[0].title()))
+guests.append('Phoenix')
+newguest = guests[-1]
+print('We will have a new guest: {0}'.format(newguest))
+print('\nI found a bigger table!')
+guests.insert(0, 'popco')
+guests.insert(3, 'pipimi')
+guests.append('lala')
+for name in guests:
+    print('I will invite {0} to have dinner'.format(name.title()))
+print("\nGod, the new table won't arrive in time!\nSorry, I can only have dinner with two guys.")
+while len(guests) > 2:
+    name = guests.pop()
+    print('Sorry dear {0}, I cannot have dinner with you'.format(name.title()))
+print()
+for name in guests:
+    print("Dear {0}, let's have dinner tonight".format(name.title()))
+del guests[:]
+print(guests)
---
+++ PythonCrashCourse/ListFunction.py
@@ -0,0 +1,19 @@
+# 首字母大写
+name = 'shen wei'
+print(name.title())
+
+# 全部大写
+print(name.upper())
+
+# 全部小写
+print(name.lower())
+
+# 删除开头空白(任何非打印字符)
+name = '  shen wei    '
+print(name.lstrip())
+
+# 删除末尾空白
+print(name.rstrip())
+
+# 删除首尾空白
+print(name.strip())
